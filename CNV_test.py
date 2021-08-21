e517360f965b741ec7770a337ae5608868527bfe
Hojun Shin <shinhoj@oregonstate.edu>
2021-08-21 16:40:04 -0700

    Update CNV_test.py

--- CNV_test.py
+++ CNV_test.py
@@ -19,49 +19,64 @@ activation = "ReLU"
 def main():
     # TEST INPUT
     kws_network = CLASS_KWS_NETWORK()
+    lossFunc = CLASS_CrossEntropySoftmax()
     # start_matrix = np.random.randint(9, size=(49, 11)) + 1
-    # NCHW
-    start_matrix = np.ones((49, 10))
-    result_matrix = kws_network.forward(start_matrix)
-    # kws.network.backward(///)
-    print("result matrix[0]: \n", result_matrix[0])
-    print("result matrix[1]: \n", result_matrix[1])
-    print("result matrix[2]: \n", result_matrix[2])
-    print("depth: ", result_matrix.shape[0])
-    print("width: ", result_matrix[0].shape[1])
-    print("height: ", result_matrix[0].shape[0])
+    start_matrix = np.random.randn(49, 10)
+
+    # Compute the scores for our 10 classes using our model
+    result = kws_network.forward(start_matrix)
+
+    # loss = lossFunc.forward(result, traindata)
+    # # accuracy
+    # acc = np.mean(np.argmax(result, axis=1)[:, np.newaxis] == traindata)
+    # # Compute gradient of Cross-Entropy Loss with respect to logits
+    # loss_grad = lossFunc.backward()
+    # # Pass gradient back through networks
+    # kws_network.backward(loss_grad)
+    # # Take a step of gradient descent
+    # kws_network.step(step_size)
+
+    print("\n\n\n-----------RESULT-----------")
+    print("Result: \n", result)
 
 
 class CLASS_CONV:
     def __init__(self):
-        # Array of Kernel Matrix
+        # Initialize parameters
         self.kernel_count = 64
-        self.kernel = np.zeros((self.kernel_count, 10, 4))
-        for i in range(self.kernel_count):
-            #self.kernel[i] = np.random.randint(9, size=(10, 4)) + 1
-            self.kernel[i] = np.ones((10, 4))
-        print("kernel matrix[0]: \n", self.kernel[0])
-
+        self.kernel_width = 4
+        self.kernel_height = 10
         self.strides = 2
+
+        # Initialize the kernel array
+        self.kernel = np.zeros(
+            (self.kernel_count, self.kernel_height, self.kernel_width))
+        # Assign random filter value
+        for i in range(self.kernel_count):
+            self.kernel[i] = np.random.randn(
+                self.kernel_height, self.kernel_width)
+
+        # Print function
+        print("CNV kernel matrix[0]: \n", self.kernel[0])
+
         # self.bias = np.ones( (1,output_dim) )*0.5
 
-    # During the forward pass, we simply compute Xw+b
     def forward(self, input):
-        print("CNV")
+        print("\n\n\n-----------CNV-----------")
         # Padding applied
         self.input = np.pad(input, ((4, 5), (1, 1)),
                             'constant', constant_values=0)
-        print("Padded matrix: \n", self.input)
+
+        # Print function
+        print("Input matrix: \n", self.input)
         print("width: ", self.input.shape[1])
         print("height: ", self.input.shape[0])
 
-        # Gather Shapes of Kernel + Image + Padding
-        kernel_width = 4
-        kernel_height = 10
-        input_height = self.input.shape[0]
+        # Input information
         input_width = self.input.shape[1]
+        input_height = self.input.shape[0]
 
-        # Shape of Output Convolution
+        # Output information
         output_width = 5
         output_height = 25
         output_matrix = np.zeros(
@@ -71,13 +86,13 @@ class CLASS_CONV:
         for i in range(self.kernel_count):
             for y in range(input_height):
                 # Exit Convolution
-                if y > input_height - kernel_height:
+                if y > input_height - self.kernel_height:
                     break
                 # Only Convolve if y has gone down by the specified Strides
                 if y % self.strides == 0:
                     for x in range(input_width):
                         # Go to next row once kernel is out of bounds
-                        if x > input_width - kernel_width:
+                        if x > input_width - self.kernel_width:
                             break
 
                         # Only Convolve if x has moved by the specified Strides
@@ -86,10 +101,13 @@ class CLASS_CONV:
                                 output_y = int(np.floor(y/self.strides))
                                 output_x = int(np.floor(x/self.strides))
                                 output_matrix[i][output_y, output_x] = (
-                                    self.kernel[i] * self.input[y: y + kernel_height, x: x + kernel_width]).sum()
+                                    self.kernel[i] * self.input[y: y + self.kernel_height, x: x + self.kernel_width]).sum()
                             except:
                                 break
 
+        print("Output matrix[0]: \n", output_matrix[0])
+        print("width: ", output_matrix[0].shape[1])
+        print("height: ", output_matrix[0].shape[0])
         return output_matrix
 
     def backward(self, grad):
@@ -111,37 +129,43 @@ class CLASS_CONV:
 
 class CLASS_D_CONV:
     def __init__(self):
-        # Array of Kernel Matrix
+        # Initialize parameters
         self.kernel_count = 64
-        self.kernel = np.zeros((self.kernel_count, 3, 3))
-        for i in range(self.kernel_count):
-            #self.kernel[i] = np.random.randint(9, size=(3, 3)) + 1
-            self.kernel[i] = np.ones((3, 3))
-        print("kernel matrix[0]: \n", self.kernel[0])
-
+        self.kernel_width = 3
+        self.kernel_height = 3
         self.strides = 1
+
+        # Initialize the kernel array
+        self.kernel = np.zeros(
+            (self.kernel_count, self.kernel_height, self.kernel_width))
+        # Assign random filter value
+        for i in range(self.kernel_count):
+            self.kernel[i] = np.random.randn(
+                self.kernel_height, self.kernel_width)
+
+        # Print Function
+        print("D-CNV kernel matrix[0]: \n", self.kernel[0])
+
         # self.bias = np.ones( (1,output_dim) )*0.5
 
-    # During the forward pass, we simply compute Xw+b
     def forward(self, input):
-        print("D-CNV")
-        print("Input: \n", input[0])
+        print("\n\n\n-----------D-CNV-----------")
         # Padding applied
         self.input = np.zeros((input.shape[0], 27, 7))
         for i in range(input.shape[0]):
             self.input[i] = np.pad(
                 input[i], ((1, 1), (1, 1)), 'constant', constant_values=0)
-        print("Padded matrix: \n", self.input[0])
+
+        # Print function
+        print("Input matrix: \n", self.input[0])
         print("width: ", self.input[0].shape[1])
         print("height: ", self.input[0].shape[0])
 
-        # Gather Shapes of Kernel + Image + Padding
-        kernel_width = 3
-        kernel_height = 3
-        input_width = self.input[0].shape[1]
-        input_height = self.input[0].shape[0]
+        # Input information
+        input_width = self.input.shape[1]
+        input_height = self.input.shape[0]
 
-        # Shape of Output Convolution
+        # Output information
         output_width = 5
         output_height = 25
         output_matrix = np.zeros(
@@ -151,13 +175,13 @@ class CLASS_D_CONV:
         for i in range(self.kernel_count):
             for y in range(input_height):
                 # Exit Convolution
-                if y > input_height - kernel_height:
+                if y > input_height - self.kernel_height:
                     break
                 # Only Convolve if y has gone down by the specified Strides
                 if y % self.strides == 0:
                     for x in range(input_width):
                         # Go to next row once kernel is out of bounds
-                        if x > input_width - kernel_width:
+                        if x > input_width - self.kernel_width:
                             break
 
                         # Only Convolve if x has moved by the specified Strides
@@ -166,10 +190,13 @@ class CLASS_D_CONV:
                                 output_y = int(np.floor(y/self.strides))
                                 output_x = int(np.floor(x/self.strides))
                                 output_matrix[i][output_y, output_x] = (
-                                    self.kernel[i] * self.input[i][y: y + kernel_height, x: x + kernel_width]).sum()
+                                    self.kernel[i] * self.input[i][y: y + self.kernel_height, x: x + self.kernel_width]).sum()
                             except:
                                 break
 
+        print("Output matrix[0]: \n", output_matrix[0])
+        print("width: ", output_matrix[0].shape[1])
+        print("height: ", output_matrix[0].shape[0])
         return output_matrix
 
     def backward(self, grad):
@@ -191,40 +218,40 @@ class CLASS_D_CONV:
 
 class CLASS_P_CONV:
     def __init__(self):
-        # Array of Kernel Matrix
+        # Initialize parameters
         self.kernel_count = 64
-        self.kernel = np.zeros((self.kernel_count, 64))
-        for i in range(self.kernel_count):
-            #self.kernel[i] = np.random.randint(9, size=(3, 3)) + 1
-            self.kernel[i] = 1
-        print("kernel matrix[0]: \n", self.kernel[0])
-
+        self.kernel_width = 1
+        self.kernel_height = 1
         self.strides = 1
+
+        # Initialize the kernel array
+        self.kernel = np.zeros((self.kernel_count, 64))
+        # Assign random filter value
+        for i in range(self.kernel_count):
+            self.kernel[i] = np.random.randn()
+
+        # Print function
+        print("P-CNV kernel matrix[0]: \n", self.kernel[0])
+
         # self.bias = np.ones( (1,output_dim) )*0.5
 
-    # During the forward pass, we simply compute Xw+b
     def forward(self, input):
-        print("P-CNV")
-        print("P_cnn: Input: \n", input[0])
-        
-        # Padding applied
+        print("\n\n\n-----------P-CNV-----------")
+
         self.input = np.zeros((input.shape[0], 25, 5))
         for i in range(input.shape[0]):
             self.input[i] = input[i]
-        print("Padded matrix: \n", self.input[0])
+
+        # Print function
+        print("Input matrix: \n", self.input[0])
         print("width: ", self.input[0].shape[1])
         print("height: ", self.input[0].shape[0])
 
-        # Gather Shapes of Kernel + Image + Padding
-        # Pointwise changed
-        kernel_width = 1
-        kernel_height = 1
-        
-        
-        input_width = self.input[0].shape[1]
-        input_height = self.input[0].shape[0]
+        # Input information
+        input_width = self.input.shape[1]
+        input_height = self.input.shape[0]
 
-        # Shape of Output Convolution
+        # Output information
         output_width = 5
         output_height = 25
         output_matrix = np.zeros(
@@ -237,13 +264,13 @@ class CLASS_P_CONV:
           # kernel 64
             for y in range(input_height):
                 # Exit Convolution
-                if y > input_height - kernel_height:
+                if y > input_height - self.kernel_height:
                     break
                 # Only Convolve if y has gone down by the specified Strides
                 if y % self.strides == 0:
                     for x in range(input_width):
                         # Go to next row once kernel is out of bounds
-                        if x > input_width - kernel_width:
+                        if x > input_width - self.kernel_width:
                             break
 
                         # Only Convolve if x has moved by the specified Strides
@@ -251,15 +278,19 @@ class CLASS_P_CONV:
                             try:
                                 output_y = int(np.floor(y/self.strides))
                                 output_x = int(np.floor(x/self.strides))
-                                #  0 1 2 3 ... 
-                                
+                                #  0 1 2 3 ...
+
                                 # calculation part
                                 for j in range(input.shape[0]):
-                                  output_matrix[i][output_y, output_x] += self.input[j][y,x] * self.kernel[i][j]
-                                  
+                                    output_matrix[i][output_y,
+                                                     output_x] += self.input[j][y, x] * self.kernel[i][j]
+
                             except:
                                 break
 
+        print("Output matrix[0]: \n", output_matrix[0])
+        print("width: ", output_matrix[0].shape[1])
+        print("height: ", output_matrix[0].shape[0])
         return output_matrix
 
     def backward(self, grad):
@@ -282,19 +313,13 @@ class CLASS_P_CONV:
 class CLASS_AVG_POOLING:
     # Forward pass is max(0,input)
     def forward(self, input):
+        print("\n\n\n-----------AVG POOLING-----------")
         # 5 x 25
         input_avg_pool = np.zeros(64)
-        print("This is area of AVG Pooling")
         for i in range(input.shape[0]):
-          print("This is array: ", np.array(input[i]))
-          print("This is sum of input: ", np.sum(input[i]))
-          print("This is size of area: ", np.size(input[i]))
-          input_avg_pool[i] = (np.sum(input[i])/np.size(input[i]))
-        # print("This is array: ", np.array(input))
-        # print("This is sum of input: ", np.sum(input))
-        # print("This is size of area: ", np.size(input))
-        # input_avg_pool = np.sum(input)/np.size(input)
-        print("This is avg_pool: ", np.array(input_avg_pool))
+            input_avg_pool[i] = (np.sum(input[i])/np.size(input[i]))
+
+        print("AVG POOLING: ", input_avg_pool)
         return input_avg_pool
     # Backward pass masks out same elements
 
@@ -308,13 +333,14 @@ class CLASS_AVG_POOLING:
 
 class CLASS_FULLY_CONNECTED:
     def __init__(self):
-        self.input = None
         self.weights = np.random.rand(64, 12)
         self.bias = np.random.rand(1, 12)
     # Forward pass is max(0,input)
+
     def forward(self, input):
-        self.input = input        
-        return np.dot(self.input, self.weights) + self.bias # calculate 64 to 10
+        self.input = input
+        # calculate 64 to 10
+        return np.dot(self.input, self.weights) + self.bias
     # Backward pass masks out same elements
 
     def backward(self, grad):
@@ -332,18 +358,17 @@ class CLASS_KWS_NETWORK:
 
         self.layers.append(CLASS_D_CONV())
         self.layers.append(CLASS_ReLU())
-        
+
         self.layers.append(CLASS_P_CONV())
         self.layers.append(CLASS_ReLU())
-        
+
         # for i in range(number_of_hidden_layers):
         #   self.layers.append(CLASS_D_CONV(hidden_dim, hidden_dim))
         #   self.layers.append(CLASS_ReLU())
         #   self.layers.append(CLASS_P_CONV(hidden_dim, hidden_dim))
         #   self.layers.append(CLASS_ReLU())
         self.layers.append(CLASS_AVG_POOLING())
-        # self.layers.append(CLASS_FULLY_CONNECTED()) 
-        
+        self.layers.append(CLASS_FULLY_CONNECTED())
 
     def forward(self, X):
         for layer in self.layers:

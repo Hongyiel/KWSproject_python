e4aeb19ab91251d492c3503d4a09825d71076147
Hongyiel Suh <suhho@oregonstate.edu>
2021-08-27 13:15:50 -0700

    all of the mfcc
    finished

--- mfcc.py
+++ mfcc.py
@@ -6,8 +6,11 @@ import librosa  # for audio related library using
 from os import listdir
 from os.path import isfile, join
 from os import walk
+import csv
+# import pandas as pd
 
-# make a function as call from above
+
+# make a audio file to matrix 49 x 10 x 1
 def make_matrix(wav_file):
     # wav_file = "go_nohash_0.wav"
     n_mfcc = 49
@@ -19,12 +22,8 @@ def make_matrix(wav_file):
     fmax = 4000
     sr = 16000
     # how to call data from librosa
-    # y, sr = librosa.load(librosa.ex('trumpet'))
     audio_data, sr = librosa.load(wav_file, sr=sr) # , offset=0.04, duration=1.0)
-    print('---audio_sr:',sr)
-    print('---audio_data:',audio_data.shape)
     audio_np = np.array(audio_data, np.float32)
-    print('audio_np:', audio_np.shape)
 
     mfcc_librosa = librosa.feature.mfcc(y=audio_data, sr=sr,
                                         win_length=win_length, hop_length=hop_length,
@@ -33,26 +32,31 @@ def make_matrix(wav_file):
                                         n_mfcc=n_mfcc, n_mels=n_mels,
                                         fmin=fmin, fmax=fmax, htk=False
                                     )
+    return mfcc_librosa
 
-    print('mfcc_librosa',mfcc_librosa.shape)
-
+# merge_matrix as 490 x 1 x 1 format
+def merge_matrix(wav_matrix):
+    new_matrix = []
+    for k in wav_matrix:
+        new_matrix.extend(k)
+        # make a format as 490 x 1 x 1
+        # export format in csv file (IMPORTANT:: format should to in line 490 x (number of files))
+    return new_matrix
 
 # get wav file from system OS
 files = [f for f in os.listdir('/Users/hongyielsuh/Documents/GitHub/KWSproject_python') if os.path.isfile(f)]
 # get files directory
+final_matrix = []
 for f in files:
-    # print(f)
     wav_constructor = os.path.splitext(f)[1]
-    # make_matrix(wav_file)
-    print(wav_constructor)
-    # do something in here
     if wav_constructor == ".wav":
-        make_matrix(f)
+        print("wav_constructor: ", wav_constructor)
+        print("file name: ", f)
+        wav_matrix = make_matrix(f)
+        # packaging the file matrix to 49 x 10 x 1 for original statement (above def will process it)
+        # distinguish the answer through the file direction that from the system input
+        final_matrix.append(merge_matrix(wav_matrix))
+        print(np.shape(final_matrix))
 
-# packaging the file matrix to 49 x 10 x 1 for original statement
-
-# distinguish the answer through the file direction that from the system input
-
-# export format in csv file (IMPORTANT:: format should to in line 490 x (number of files))
-
-
+np.savetxt('wav_data.csv', final_matrix, delimiter=',') 
+# set matrix for import main python file

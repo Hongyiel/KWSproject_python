b80de0273268644d2cef372688a4c718e098e1ab
Hongyiel Suh <suhho@oregonstate.edu>
2021-08-28 17:42:54 -0700

    some fixed
        # this is for labelling answer sheet
        x = np.array([1], dtype='f')
        new_matrix.extend(x)

--- speech_data_csv/merged_csv.py
+++ speech_data_csv/merged_csv.py
@@ -1,12 +1,30 @@
 # this file for merge dataset from speech data
 
-import os
+import csv
 import glob
-import pandas as pd
+import os
+import numpy as np
 
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-#combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+# bring all csv file
+# file file name including "go" then add 1 column and replace it
+
+
+files = [f for f in os.listdir('/Users/hongyielsuh/Documents/GitHub/KWSproject_python/speech_data_csv') if os.path.isfile(f)]
+
+for f in files:
+    key_word_constructor = os.path.splitext(f)[0]
+    print(key_word_constructor)
+    
+    if key_word_constructor in "wav_data_yes":
+        print("yes!")
+    if key_word_constructor in "wav_data_go":
+        with open(f, 'r') as csvoutput:
+            print(np.shape(f))
+
+    # if key_word_constructor in "wav_data_left":
+    # if key_word_constructor in "wav_data_down":
+    # if key_word_constructor in "wav_data_up":
+
+    # if key_word_constructor in "wav_data_no":
+    # if key_word_constructor in "wav_data_off":
+

a11d1ed38fbdb5c3601630fbb71250685a08a1a1
Hongyiel Suh <suhho@oregonstate.edu>
2021-08-28 18:43:50 -0700

    final dataset

--- speech_data_csv/merged_csv.py
+++ speech_data_csv/merged_csv.py
@@ -4,16 +4,23 @@ import csv
 import glob
 import os
 import numpy as np
+import pandas as pd
+
 
 # bring all csv file
 # file file name including "go" then add 1 column and replace it
 
-
+path = '/Users/hongyielsuh/Documents/GitHub/KWSproject_python/speech_data_csv'
 files = [f for f in os.listdir('/Users/hongyielsuh/Documents/GitHub/KWSproject_python/speech_data_csv') if os.path.isfile(f)]
+frames= []
 
 for f in files:
-    key_word_constructor = os.path.splitext(f)[0]
-    print(key_word_constructor)
+    
+    key_word_constructor = os.path.splitext(f)[1]
+    if key_word_constructor == ".csv":
+        data = pd.read_csv(f,header=None)
+        
+        frames.append(data)
     
     # if key_word_constructor in "wav_data_down 10"
     # if key_word_constructor in "wav_data_go"  1:
@@ -28,5 +35,33 @@ for f in files:
     
     # if +silence 11
     # if unknown 12
+    
+df = pd.concat(frames)
+print(df)
+
+df.to_csv("data_set.csv")
+# merging the files
+# joined_files = os.path.join(path, "wav_data_*.csv")
+# # A list of all joined files is returned
+# joined_list = glob.glob(joined_files)
+# # Finally, the files are joined
+
+# df = pd.concat((pd.read_csv(file).assign(filename = file) for file in joined_list), ignore_index=True)
+# print(df)
+# print("process finished...")
 
 
+# interesting_files = glob.glob("/Users/hongyielsuh/Documents/GitHub/KWSproject_python/speech_data_csv/*.csv") 
+# df = pd.concat((pd.read_csv(f, header = None) for f in interesting_files))
+# # df.to_csv("output.csv")
+
+# print(df)
+
+# data1 = pd.read_csv("wav_data_up.csv",header=None)
+# data2 = pd.read_csv("wav_data_on.csv",header=None)
+
+
+# frames = [data1, data2]
+# df = pd.concat(frames)
+
+# print(df)
